5501b4a3cf1dd9b0eb97db0d21375a3c422c9b33
Juan Sanzone OLX <juan.sanzone@olx.com>
2015-04-11 12:14:23 -0300

    Improvements Home

--- js/binds.js
+++ js/binds.js
@@ -1,6 +1,28 @@
+var collectorCustom = {
+	user: "",
+	repo: "",
+	text: "Send Feedback", 
+	colorText: "#FFF", 
+	colorBack: "215EBB"
+};
+
 function getTemplate(templatePath, data) 
 {
 	return window['JST'][templatePath](data);
+}
+
+function updateColor() {
+	$("#user_button").css("cssText", 
+		"background-color: " + collectorCustom.colorBack + " !important;" + 
+		"color: " + collectorCustom.colorText + " !important"
+	);
+}
+
+function doHighlight() 
+{
+	$('pre code').each(function(i, block) {
+    	hljs.highlightBlock(block);
+  	});
 }
 
 function homeBinds() 
@@ -13,15 +35,47 @@ function homeBinds()
 	    maxPaletteSize: 10,
 	    preferredFormat: "hex",
 	    move: function (color) {
-	    	$("#user_button").css("cssText", "background-color: " + color + " !important;");
+	    	collectorCustom.colorBack = color;
+	    	updateColor();
 	    },
 	    change: function(color) {
-        	$("#user_button").css("cssText", "background-color: " + color + " !important;");
+        	collectorCustom.colorBack = color;
+        	updateColor();
+    	}
+	});
+
+	$("#colorpickerText").spectrum({
+	    color: "#FFF",
+	    showInput: true,
+	    className: "full-spectrum",
+	    showInitial: true,
+	    maxPaletteSize: 10,
+	    preferredFormat: "hex",
+	    move: function (color) {
+	    	collectorCustom.colorText = color;
+	    	updateColor();
+	    },
+	    change: function(color) {
+        	collectorCustom.colorText = color;
+        	updateColor();
     	}
 	});
 
 	$('#tab_text').keyup(function() {
-		var updatedText = $(this).val();
-		$('#user_button').html(updatedText);
+		$('#user_button').html($(this).val());
+		collectorCustom.text = $(this).val();
+	});
+
+	$('#git_username').keyup(function() {
+		collectorCustom.user = $(this).val();
+	});
+
+	$('#git_repo').keyup(function() {
+		collectorCustom.repo = $(this).val();
+	});
+
+	$('#tabCode').click(function() {
+		$('#collectorCode').html(JSON.stringify(collectorCustom));
+		doHighlight();
 	});
 }
